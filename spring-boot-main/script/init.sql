e7543b24c9e507f28b89fcccf7db55b890323a9a
pengb <pengb@sunline.cn>
2019-08-21 14:50:07 +0800

    添加sql

--- spring-boot-main/script/init.sql
+++ spring-boot-main/script/init.sql
@@ -1,5 +1,6 @@
 DROP TABLE IF EXISTS auth_process_control;
 create table  auth_process_control (
-  pc_auth_trxn_code varchar (7) not null comment '授权交易代码 ',
-  pc_step           int          not null comment '执行步骤',
+  pc_auth_trxn_code varchar(7)   not null comment '授权交易代码 ',
+  pc_step           int           not null comment '执行步骤',
+  pc_component_bean varchar(100) not null comment '授权组件bean',
 )
